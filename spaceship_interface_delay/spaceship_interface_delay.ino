baa4a47b6fa82d607452aed52ab6133527eb5077
Carrie Iggulden <deusexmachina@Carries-MacBook-Air.local>
2017-09-22 21:22:20 -0400

    blink plus delay

---
+++ spaceship_interface_delay/spaceship_interface_delay.ino
@@ -0,0 +1,30 @@
+int switchState = 0; 
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(5, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(3, OUTPUT);
+pinMode(2, INPUT);
+} 
+
+void loop() {
+  // put your main code here, to run repeatedly:
+switchState = digitalRead(2);
+if (switchState == LOW) {
+  digitalWrite(3, HIGH); // green LED
+  digitalWrite(4, LOW); //  red LED
+  digitalWrite(5, LOW); // red LED
+  
+} else { // the button is pressed:
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+
+  delay(50); 
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(50); 
+
+}
+}
